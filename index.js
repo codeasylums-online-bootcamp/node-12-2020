b7148407436cec0d3031a4ebe7047e5658ae28be
satyarth.ojha <satyarth.ojha@grabpay.com>
2020-12-05 23:04:22 +0530

    added mongodb connection

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const userRoute = require('./routes/users')
 
 const app = express()
 const port = 3000
 
+const {DB_NAME, DB_USER_NAME, DB_USER_PASSWORD} = process.env
+mongoose.connect(`mongodb+srv://${DB_USER_NAME}:${DB_USER_PASSWORD}@codeasylums-nodejs.h2x8l.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,(err)=>{
+    if(err){
+        console.log("DB Connection failed")
+    }
+    else{
+        console.log("Atlas DB connection successful")
+    }
+})
 
 const logger = morgan('dev')
 
 app.use(logger)
 
-app.use('/user',userRoute)
+app.use('/user',userRoute) // /user/abc
 
 app.use('*',(req,res,next)=>{
     res.send("Endpoint is not present 404").status(404)
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const userID = require('../controllers/users').userID
-const userHome = require('../controllers/users').userHome
+const {userID, userHome} = require('../controllers/users')
 
 // /user/abc -> /abc
 router.get('/',userHome) // path = "/"
